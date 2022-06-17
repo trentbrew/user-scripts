950e6276bd0eaccdb237239076eeff43454b36a8
Trent Brew <32501733+trentbrew@users.noreply.github.com>
2022-06-16 19:09:03 -0500

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,29 @@
+// --- remove distracting youtube suggestions ---
+
+var primary = document.getElementById('page-manager');
+
+// hide recommendations
+primary.style.opacity = '0';
+primary.style.pointerEvents = 'none';
+
+// hide closed drawer
+var drawer = document.querySelectorAll('ytd-mini-guide-renderer')[0];
+drawer.style.opacity = '0';
+
+setTimeout(() => {
+  // close drawer
+  document.getElementById('guide-button').click();
+}, 100);
+
+// press tick key to un-hide recommendations
+window.addEventListener('keyup', (e) => {
+  if (e.keyCode == 192) {
+    if (primary.style.opacity == '0') {
+      primary.style.opacity = '1';
+      primary.style.pointerEvents = 'all';
+    } else {
+      primary.style.opacity = '0';
+      primary.style.pointerEvents = 'none';
+    }
+  }
+});

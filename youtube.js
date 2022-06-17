7706b1eb114ee58fc386bf2275fc9957e68e75be
Trent Brew <32501733+trentbrew@users.noreply.github.com>
2022-06-16 19:39:49 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -3,15 +3,16 @@
 var primary = document.getElementById('page-manager');
 
 // hide recommendations
+
 primary.style.opacity = '0';
 primary.style.pointerEvents = 'none';
 
-// hide closed drawer
+// hide drawer
+
 var drawer = document.querySelectorAll('ytd-mini-guide-renderer')[0];
 drawer.style.opacity = '0';
 
 setTimeout(() => {
-  // close drawer
   document.getElementById('guide-button').click();
 }, 100);
 
